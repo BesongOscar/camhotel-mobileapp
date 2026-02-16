188265f318e7cce3091c09f63d9644444a5cfb98
Besong Oscar <wildeo963@gmail.com>
2026-02-16 15:10:14 +0100

    theme folder setup for color, typography, spacing- implemented

--- app/(homeExtras)/hotelDetailScreen.tsx
+++ app/(homeExtras)/hotelDetailScreen.tsx
@@ -2,6 +2,7 @@
 import AppBar from "@/components/(homeExtras)/appBar";
 import Button from "@/components/button";
 import hotels from "@/constants/hotelCard";
+import { amenities } from "@/constants/amenities";
 import { hotelDetailStyles as styles } from "@/styles/app/(homeExtras)/hotelDetailScreen";
 import {
   Ionicons,
@@ -92,14 +93,7 @@ export default function HotelDetailScreen() {
   const moreCount = gallery.length > 4 ? gallery.length - 4 : 0;
 
   // amenities
-  const amenities = [
-    { key: "breakfast", label: "Breakfast", icon: "food" },
-    { key: "conference", label: "Conference hall", icon: "briefcase-outline" },
-    { key: "restaurant", label: "Restaurant", icon: "silverware-fork-knife" },
-    { key: "gym", label: "Gym", icon: "dumbbell" },
-    { key: "parking", label: "Free Parking", icon: "parking" },
-    { key: "kids", label: "Kids' club", icon: "gamepad-variant" },
-  ];
+  
 
   return (
     <SafeAreaView style={styles.safe} edges={["top"]}>
---
+++ constants/amenities.ts
@@ -0,0 +1,8 @@
+export const amenities = [
+    { key: "breakfast", label: "Breakfast", icon: "food" },
+    { key: "conference", label: "Conference hall", icon: "briefcase-outline" },
+    { key: "restaurant", label: "Restaurant", icon: "silverware-fork-knife" },
+    { key: "gym", label: "Gym", icon: "dumbbell" },
+    { key: "parking", label: "Free Parking", icon: "parking" },
+    { key: "kids", label: "Kids' club", icon: "gamepad-variant" },
+  ];
